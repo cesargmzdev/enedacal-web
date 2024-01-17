d7a8d8c9706fd8fa579e681acd7c925d21a53f26
César Gómez <cesargomez015@gmail.com>
2024-01-17 22:14:37 +0100

    changed cursors

--- src/components/subcomponents/CarouselButtons.jsx
+++ src/components/subcomponents/CarouselButtons.jsx
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
+import LeftArrow from '../../assets/LeftArrow.svg';
+import RightArrow from '../../assets/RightArrow.svg';
 
 const PrevButton = ({onClick}) => {
   return (
-    <button onClick={onClick} type="button" className='absolute h-full w-1/5'><svg xmlns="http://www.w3.org/2000/svg" className='mix-blend-difference absolute left-16 top-[49%]' width="30" height="30" fill="white" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="160 208 80 128 160 48" fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></polyline></svg></button>
+    <button onClick={onClick} type="button" className='absolute h-full w-1/5' style={{cursor: `url(${LeftArrow}) 8 8, auto`}} />
   );
 };
 
 const NextButton = ({onClick}) => {
   return (
-    <button onClick={onClick} type="button" className='absolute h-full right-0 w-1/5'><svg xmlns="http://www.w3.org/2000/svg" className='mix-blend-difference absolute right-16 top-[49%]' width="30" height="30" fill="white" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><polyline points="96 48 176 128 96 208" fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></polyline></svg></button>
+    <button onClick={onClick} type="button" className='absolute h-full right-0 w-1/5 mix-blend-difference' style={{cursor: `url(${RightArrow}) 8 8, auto`}} />
   );
 };
 
