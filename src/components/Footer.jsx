0a22ba4aa2fed96f84cd80222f453cd32d819c93
César Gómez <cesargomez015@gmail.com>
2024-04-22 15:54:39 +0200

    Update copyright information in Footer component and replaces RUKATYPE_work.webp image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const Footer = ({classNames}) => {
     const Year = new Date().getFullYear();
     return (
-        <footer className={`text-center text-sm bg-black text-white ${classNames}`}>Copyright &copy; {Year} website designed by Nico Dacal, developed by C&eacute;sar G&oacute;mez - All rights reserved</footer>
+        <footer className={`text-center text-sm bg-black text-white ${classNames}`}>Copyright &copy; {Year} - Website designed by Nico Dacal, developed by C&eacute;sar G&oacute;mez - All&nbsp;rights&nbsp;reserved</footer>
     );
 };
 
